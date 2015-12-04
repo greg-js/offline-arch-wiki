84d8d6fa3603e9bc2e7f80f8031ad4e658fe7eb1
Gregory Scheerlinck <gregory.scheerlinck@gmail.com>
2015-12-04 20:51:17 +0000

    added scrape.changes

--- test/scrape.js
+++ test/scrape.js
@@ -139,4 +139,30 @@ describe('scrape.js', function() {
       done();
     });
   });
+
+  describe('scrape.changes', function() {
+    before(function(done) {
+      var mockRes = mock('<html><body><div id="mw-content-text"><div class="mw-changeslist"><h4>4 December 2015</h4><div><table class="mw-enhanced-rc mw-changeslist-ns0-SSMTP mw-changeslist-line-not-watched"><tbody><tr><td class="mw-enhanced-rc"><span class="mw-enhancedchanges-arrow-space"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;13:58&nbsp;</td><td> <span class="mw-title"><a href="/index.php/TEST1" title="Test 1" class="mw-changeslist-title">TEST 1</a></span>‎ (<a href="/index.php?title=test1&amp;curid=5840&amp;diff=410868&amp;oldid=393600" tabindex="1">diff</a> | <a href="/index.php?title=SSMTP&amp;curid=5840&amp;action=history" title="SSMTP">hist</a>) <span class="mw-changeslist-separator">. .</span> <span dir="ltr" class="mw-plusminus-pos" title="5,089 bytes after change">(+425)</span>‎ <span class="mw-changeslist-separator">. .</span>  <a href="/index.php?title=User:Thomastc&amp;action=edit&amp;redlink=1" class="new mw-userlink" title="User:Thomastc (page does not exist)">Thomastc</a> <span class="mw-usertoollinks">(<a href="/index.php?title=User_talk:Thomastc&amp;action=edit&amp;redlink=1" class="new" title="User talk:Thomastc (page does not exist)">Talk</a> | <a href="/index.php/Special:Contributions/Thomastc" title="Special:Contributions/Thomastc">contribs</a>)</span> <span class="comment">(Point to App Passwords for Gmail)</span></td></tr></tbody></table><table class="mw-enhanced-rc mw-changeslist-ns0-NFS mw-changeslist-line-not-watched"><tbody><tr><td class="mw-enhanced-rc"><span class="mw-enhancedchanges-arrow-space"></span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;18:46&nbsp;</td><td> <span class="mw-title"><a href="/index.php/TEST2" title="Test 2" class="mw-changeslist-title">Test 2</a></span>‎ (<a href="/index.php?title=NFS&amp;curid=15069&amp;diff=410872&amp;oldid=408860" tabindex="5">diff</a> | <a href="/index.php?title=NFS&amp;curid=15069&amp;action=history" title="NFS">hist</a>) <span class="mw-changeslist-separator">. .</span> <span dir="ltr" class="mw-plusminus-pos" title="19,396 bytes after change">(+64)</span>‎ <span class="mw-changeslist-separator">. .</span>  <a href="/index.php?title=User:Aw24&amp;action=edit&amp;redlink=1" class="new mw-userlink" title="User:Aw24 (page does not exist)">Aw24</a> <span class="mw-usertoollinks">(<a href="/index.php?title=User_talk:Aw24&amp;action=edit&amp;redlink=1" class="new" title="User talk:Aw24 (page does not exist)">Talk</a> | <a href="/index.php/Special:Contributions/Aw24" title="Special:Contributions/Aw24">contribs</a>)</span> <span class="comment">(<a href="/index.php/NFS#NetworkManager_dispatcher" title="NFS">→</a>‎<span dir="auto"><span class="autocomment">NetworkManager dispatcher: </span> add warning on noauto</span>)</span></td></tr></tbody></table></div></div></body></html>');
+      mockery.enable(mockeryConfig);
+      mockery.registerAllowable('../lib/scrape', true);
+      mockery.registerMock('request-promise', mockRes);
+      done();
+    });
+
+    it('should return an array of article objects', function(done) {
+      scrape.changes('some-url/index.php?changesetc').then(function(changesArray) {
+        expect(changesArray).to.be.an('array');
+        expect(changesArray.length).to.equal(2);
+        expect(changesArray[0].url).to.equal('https://wiki.archlinux.org/index.php/TEST1');
+        expect(changesArray[1].url).to.equal('https://wiki.archlinux.org/index.php/TEST2');
+        done();
+      });
+    });
+
+    after(function(done) {
+      mockery.disable();
+      mockery.deregisterAll();
+      done();
+    });
+  });
 });
